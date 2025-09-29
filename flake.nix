19b80c0d0836c929d37921c5b5364ebe5e4b1160
glen@starbak <glen@accur8software.com>
2025-09-28 21:31:14 -0400

    Update nix-pins to latest

--- flake.nix
+++ flake.nix
@@ -2,41 +2,34 @@
   description = "codegen project";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    devshell.url = "github:numtide/devshell";
-    a8-scripts.url = "github:fizzy33/a8-scripts";
-    a8-scripts.inputs.nixpkgs.follows = "nixpkgs";
+    nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
+    nixpkgs.follows = "nix-pins/nixpkgs";
+    devshell.follows = "nix-pins/devshell";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell, a8-scripts }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ devshell.overlays.default ];
-        };
+  outputs = { self, nixpkgs, nix-pins, devshell }:
+    {
+      devShells = nix-pins.lib.forEachSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              nix-pins.overlays.default
+              devshell.overlays.default
+            ];
+          };
 
-        # Java/Scala setup
-        my-java = pkgs.openjdk11_headless;
-        my-scala = pkgs.scala.override { jre = my-java; };
-        my-sbt = pkgs.sbt.override { jre = my-java; };
-        my-ammonite = pkgs.ammonite_2_13.override { jre = my-java; };
-
-      in {
-        devShells.default = pkgs.devshell.mkShell {
-          name = "codegen";
-
-          packages = [
-            a8-scripts.packages.${system}.a8-scripts
-            my-ammonite
-            my-java
-            my-sbt
-            my-scala
-            pkgs.python3
-            pkgs.gnupg
-          ];
-        };
-      }
-    );
+          # Java/Scala setup with JDK 11
+          my-java = pkgs.openjdk11_headless;
+        in {
+          default = nix-pins.lib.shells.scala {
+            inherit pkgs;
+            jdk = my-java;
+            extraInputs = [
+              pkgs.python3
+              pkgs.gnupg
+            ];
+          };
+        });
+    };
 }
