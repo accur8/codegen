d31f527dde9fe77e045f0fcc4bc93f915cc8b99c
glen@starbak <glen@accur8software.com>
2025-09-29 10:15:41 -0400

    more nix-pins enhancements

--- flake.nix
+++ flake.nix
@@ -3,33 +3,22 @@
 
   inputs = {
     nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
-    nixpkgs.follows = "nix-pins/nixpkgs";
-    devshell.follows = "nix-pins/devshell";
   };
 
   outputs = { self, nixpkgs, nix-pins, devshell }:
     {
       devShells = nix-pins.lib.forEachSystem (system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              nix-pins.overlays.default
-              devshell.overlays.default
-            ];
-          };
-
-          # Java/Scala setup with JDK 11
-          my-java = pkgs.openjdk11_headless;
+          pkgs = nix-pins.pkgsFor system;
+          # Add any project-specific packages or overrides here
         in {
           default = nix-pins.lib.shells.scala {
-            inherit pkgs;
-            jdk = my-java;
             extraInputs = [
               pkgs.python3
               pkgs.gnupg
             ];
           };
-        });
+        }
+      );
     };
 }
