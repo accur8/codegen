d944b6aa99e18f3079f6fea4bc98178f312bca80
glen@starbak <glen@accur8software.com>
2025-10-03 22:00:51 -0400

    migrate the nix-pins composable shells

--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "codegen project";
 
   inputs = {
-    nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
+    # Using local path for testing new composable shells
+    nix-pins.url = "path:/Users/glen/code/accur8/nix-pins";
+    # When ready, switch back to: "git+ssh://git@git.accur8.net/a8/nix-pins";
+    nixpkgs.follows = "nix-pins/nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nix-pins, devshell }:
+  outputs = { self, nix-pins, ... }:
     {
-      devShells = nix-pins.lib.forEachSystem (system:
-        let
-          pkgs = nix-pins.pkgsFor system;
-          # Add any project-specific packages or overrides here
-        in {
-          default = nix-pins.lib.shells.scala {
-            extraInputs = [
-              pkgs.python3
+      devShells = nix-pins.lib.composeDevShells {
+        default = {pkgs, frags, ...}: [
+          (frags.scala_213 {
+            extra = [
               pkgs.gnupg
             ];
-          };
-        }
-      );
+          })
+          (frags.aiCodingAssistants { })  # Already includes python312
+        ];
+      };
     };
 }
