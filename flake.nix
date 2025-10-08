183af6a3e1b40fc62f0c05cf818941532dd8928b
glen@starbak <glen@accur8software.com>
2025-10-08 09:43:41 -0400

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,17 +5,20 @@
     nix-pins.url = "git+ssh://git@git.accur8.net/a8/nix-pins";
   };
 
-  outputs = { self, nix-pins, ... }:
+  outputs =
+    { self, nix-pins, ... }:
     {
       devShells = nix-pins.lib.composeDevShells {
-        default = {pkgs, frags, ...}: [
-          (frags.scala_213 {
-            extra = [
-              pkgs.gnupg
-            ];
-          })
-          (frags.aiCodingAssistants { })  # Already includes python312
-        ];
+        default =
+          { pkgs, frags, ... }:
+          [
+            (frags.scala_213 {
+              extra = [
+                pkgs.gnupg
+              ];
+            })
+            (frags.aiCodingAssistants { }) # Already includes python312
+          ];
       };
     };
 }
